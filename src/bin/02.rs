ffe9b5d1cc7ea4f4dc77ad5559adcff99cb27ba4
Aaron Wirick <aaron.wirick@cortex.io>
2024-12-02 20:32:07 -0800

    Day 2 - Part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -3,7 +3,9 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
+use thiserror::Error;
 use adv_code_2024::*;
+use crate::LevelError::UnsafeLevel;
 
 const DAY: &str = "02";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
@@ -17,6 +19,15 @@ const TEST: &str = "\
 1 3 6 7 9
 ";
 
+#[derive(Error, Debug)]
+pub enum LevelError {
+    #[error("Unsafe level at index {cur} and {next}")]
+    UnsafeLevel { cur: usize, next: usize },
+}
+
+type LevelResult = Result<(), LevelError>;
+
+
 fn main() -> Result<()> {
     start_day(DAY);
 
@@ -25,7 +36,8 @@ fn main() -> Result<()> {
 
 
     // A safe level is monotonically increasing or decreasing AND increase by at least 1 but not more than 3 between each number
-    fn is_safe_level(level: &[i64]) -> Result<bool> {
+    // When we are unsafe, we return an error with the unsafe index
+    fn is_safe_level(level: &[i64]) -> LevelResult {
         let mut cur = level[0];
         let mut next = level[1];
 
@@ -33,7 +45,7 @@ fn main() -> Result<()> {
         let direction = (next - cur).signum();
 
         if direction == 0 {
-            return Ok(false)
+            return Err(UnsafeLevel { cur: 0, next: 1 })
         }
 
         for i in 1..level.len() {
@@ -45,16 +57,16 @@ fn main() -> Result<()> {
 
             // Must maintain the same direction
             if dir != direction {
-                return Ok(false)
+                return Err(UnsafeLevel { cur: i - 1, next: i })
             }
 
             // Must change by at least 1 but not more than 3
             if (change < 1) || (change > 3) {
-                return Ok(false)
+                return Err(UnsafeLevel { cur: i - 1, next: i })
             }
         }
 
-        Ok(true)
+        LevelResult::Ok(())
     }
 
 
@@ -71,9 +83,12 @@ fn main() -> Result<()> {
                 level.push(part.parse::<i64>()?);
             }
 
-            if is_safe_level(&level)? {
-                // println!("{:?} was safe", level);
-                safe += 1;
+            match is_safe_level(&level) {
+                LevelResult::Ok(_) => {
+                    // println!("{:?} was safe", level);
+                    safe += 1;
+                }
+                Err(_) => {}
             }
         }
 
@@ -88,17 +103,63 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+
+    fn copy_without_index(level: &[i64], index: usize) -> Vec<i64> {
+        let mut copy = Vec::new();
+        for i in 0..level.len() {
+            if i != index {
+                copy.push(level[i]);
+            }
+        }
+        copy
+    }
+
+
+    // Similar to part 1 but also allows safely if there is one anomaly
+    fn is_mostly_safe(levels: &[i64]) -> LevelResult {
+        for i in 0..levels.len() {
+            let copy = copy_without_index(levels, i);
+            match is_safe_level(&copy) {
+                LevelResult::Ok(_) => {
+                    return LevelResult::Ok(());
+                }
+                Err(_) => {}
+            }
+        }
+
+        Err(UnsafeLevel { cur: 0, next: 1 })
+    }
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        // We can process each row independently
+        let mut safe: usize = 0;
+
+        for line in reader.lines() {
+            let line = line?;
+            let mut parts = line.split_whitespace();
+            let mut level = Vec::new();
+            for part in parts {
+                level.push(part.parse::<i64>()?);
+            }
+
+            match is_mostly_safe(&level) {
+                LevelResult::Ok(_) => {
+                    // println!("{:?} was safe", level);
+                    safe += 1;
+                }
+                Err(_) => {}
+            }
+        }
+        Ok(safe)
+    }
+
+    assert_eq!(4, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
